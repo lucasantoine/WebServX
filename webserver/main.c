a28cd76c02725d6bdfe9cb43e7f5d59c74f51a69
Elkios_ <pomier.mathys@outlook.fr>
2020-01-31 11:55:19 +0100

    Serveur TCP Simple

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,34 @@
-# include <stdio.h>
-# include <string.h>
+#include <stdio.h>
+#include <sys/types.h>     
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include "socket.h"
 
-int main (int argc , char ** argv){
-    /* Arnold Robbins in the LJ of February ’95 , describing RCS */
-    if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
-        printf (" Don ’t Panic !\n ");
-        return 42;
-    } 
-    printf (" Need an advice ?\n ");
+int main (/*int argc , char ** argv*/){
+   
+    int socket_serveur = creer_serveur(8080);
+    int socket_client ;
+	char client_message[80];
+	int size = 0;
+	while(1){
+	    socket_client = accept(socket_serveur, NULL, NULL);
+		if(socket_client == -1){
+		    perror("accept");
+		    return -1;
+		    /* traitement d ’ erreur */
+		}
+		/* On peut maintenant dialoguer avec le client */
+		const char *message_bienvenue[11] = {"*---------------------------------------------------------------*\n", "Bonjour et bienvenue sur WebServX !\n", "Il s'agit de la page par défaut de WebServX, si vous la voyez\n", "cela signifie que tout fonctionne.\n", "Pour la changer, nous vous invitons a regarder la documentation.\n", "WebServX est un serveur web open source crée dans le cadre\n", "du cours de Programmation Système Avancé à l'IUT A de Lille.\n", "Ce projet a été réalisé par ANTOINE Lucas et POMIER Mathys\n", "et supervisé par HAUSPIE Michael, PLACE Jean-Marie, RIQUET Damien\n", "et BEAUFILS Bruno. Projet lancé le 31/01/2020.\n", "*---------------------------------------------------------------*\n"};
+		sleep(1);
+		for(int i = 0; i < 11; i++){
+			write(socket_client , message_bienvenue[i] , strlen(message_bienvenue[i]));
+		}
+		while((size = read(socket_client, client_message, 80)) != -1){
+			write(socket_client, client_message, size);
+		}
+
+	}
     return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/types.h>     
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include "socket.h"
+
+
+int creer_serveur(int port){
+     int socket_serveur;
+    socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_serveur == -1){
+        /* traitement de l ’ erreur */
+        perror ("socket_serveur");
+        return -1;
+    }
+
+    struct sockaddr_in saddr;
+    saddr.sin_family = AF_INET; /* Socket ipv4 */
+    saddr.sin_port = htons(port); /* Port d ’e coute */
+    saddr.sin_addr.s_addr = INADDR_ANY; /* e coute sur toutes les interfaces */
+
+    /* Utilisation de la socket serveur */
+    if (bind(socket_serveur, (struct sockaddr *)& saddr, sizeof(saddr)) == -1){
+        perror("bind socker_serveur");
+        return -1;
+        /* traitement de l ’ erreur */
+    }
+    if (listen(socket_serveur, 10) == -1){
+        perror("listen socket_serveur");
+        return -1;
+        /* traitement d ’ erreur */
+    }
+	return socket_serveur;
+}
